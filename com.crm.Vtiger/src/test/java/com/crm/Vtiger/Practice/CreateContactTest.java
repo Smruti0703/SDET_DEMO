4f6c88323bc2b1b44e2a98d60df791130392303a
gedi <gedi@LAPTOP-A6SHHES2>
2022-03-28 15:08:42 +0530

    contact modified

--- com.crm.Vtiger/src/test/java/com/crm/Vtiger/Practice/CreateContactTest.java
+++ com.crm.Vtiger/src/test/java/com/crm/Vtiger/Practice/CreateContactTest.java
@@ -6,12 +6,13 @@ public class CreateContactTest {
 	@Test(groups="SmokeTest")
 	public void createContact() {
 		System.out.println("Contact created");
+		System.out.println("create modified");
 	}
 @Test(groups="RegressionTest")
 public void deletecontact() {
 	System.out.println("Contact deleted");
 }
 	
-//commented added.	
+	
 	
 }
